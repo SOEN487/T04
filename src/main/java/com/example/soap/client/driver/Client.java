619b68015f50819eda4c31666df83361147b2508
Konstantinos Psimoulis <kosta@KonstannossMini.local>
2021-01-29 21:17:07 -0500

    finished commit

--- src/main/java/com/example/soap/client/driver/Client.java
+++ src/main/java/com/example/soap/client/driver/Client.java
@@ -5,6 +5,9 @@ import com.example.soap.client.service.*;
 public class Client {
 
     public static void main(String[] args) {
-
+        HelloWorldImplService service = new HelloWorldImplService();
+        HelloWorld port = service.getHelloWorldImplPort();
+        String result = port.sayHelloWorld("World");
+        System.out.println(result);
     }
 }
--- src/main/java/com/example/soap/service/HelloWorld.java
+++ src/main/java/com/example/soap/service/HelloWorld.java
@@ -1,4 +1,14 @@
 package com.example.soap.service;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 
+/**
+ * Interface for our HelloWorld web service
+ */
+@WebService
+@SOAPBinding
 public interface HelloWorld {
+    @WebMethod
+    String sayHelloWorld(String content);
 }
