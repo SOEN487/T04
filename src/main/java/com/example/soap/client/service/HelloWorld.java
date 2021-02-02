36daef0a512dd9a87e09dbe6723bc93cf7272e30
Konstantinos Psimoulis <kosta@noima.com>
2021-02-02 17:10:59 -0500

    changing order of client

--- src/main/java/com/example/soap/client/service/HelloWorld.java
+++ src/main/java/com/example/soap/client/service/HelloWorld.java
@@ -28,21 +28,6 @@ public interface HelloWorld {
      * 
      * @param arg0
      * @return
-     *     returns com.example.soap.client.service.StudentData
-     */
-    @WebMethod
-    @WebResult(targetNamespace = "")
-    @RequestWrapper(localName = "getStudent", targetNamespace = "http://service.soap.example.com/", className = "com.example.soap.client.service.GetStudent")
-    @ResponseWrapper(localName = "getStudentResponse", targetNamespace = "http://service.soap.example.com/", className = "com.example.soap.client.service.GetStudentResponse")
-    @Action(input = "http://service.soap.example.com/HelloWorld/getStudentRequest", output = "http://service.soap.example.com/HelloWorld/getStudentResponse")
-    public StudentData getStudent(
-        @WebParam(name = "arg0", targetNamespace = "")
-        String arg0);
-
-    /**
-     * 
-     * @param arg0
-     * @return
      *     returns java.lang.String
      */
     @WebMethod
@@ -51,6 +36,21 @@ public interface HelloWorld {
     @ResponseWrapper(localName = "sayHelloWorldResponse", targetNamespace = "http://service.soap.example.com/", className = "com.example.soap.client.service.SayHelloWorldResponse")
     @Action(input = "http://service.soap.example.com/HelloWorld/sayHelloWorldRequest", output = "http://service.soap.example.com/HelloWorld/sayHelloWorldResponse")
     public String sayHelloWorld(
+        @WebParam(name = "arg0", targetNamespace = "")
+        String arg0);
+
+    /**
+     * 
+     * @param arg0
+     * @return
+     *     returns com.example.soap.client.service.StudentData
+     */
+    @WebMethod
+    @WebResult(targetNamespace = "")
+    @RequestWrapper(localName = "getStudent", targetNamespace = "http://service.soap.example.com/", className = "com.example.soap.client.service.GetStudent")
+    @ResponseWrapper(localName = "getStudentResponse", targetNamespace = "http://service.soap.example.com/", className = "com.example.soap.client.service.GetStudentResponse")
+    @Action(input = "http://service.soap.example.com/HelloWorld/getStudentRequest", output = "http://service.soap.example.com/HelloWorld/getStudentResponse")
+    public StudentData getStudent(
         @WebParam(name = "arg0", targetNamespace = "")
         String arg0);
 
